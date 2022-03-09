4b8ffedfd19ae2d06a2974e06f4836e91e2dc13d
nuilewis <lewisnuikweh@gmail.com>
2022-03-09 14:43:58 +0100

    Refactored StatsCard and Recommendation card to separate widgets

--- lib/screens/homescreen/homescreen.dart
+++ lib/screens/homescreen/homescreen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sickler/constants.dart';
-import 'package:sickler/global%20components/appbar.dart';
+import 'package:sickler/global_components/appbar.dart';
+import 'package:sickler/screens/homescreen/components/stats_card.dart';
+
+import 'components/recommendation_card.dart';
 
 class SicklerHomeScreen extends StatefulWidget {
   const SicklerHomeScreen({Key? key}) : super(key: key);
@@ -49,56 +52,10 @@ class _SicklerHomeScreenState extends State<SicklerHomeScreen> {
                 ),
               ),
               const SizedBox(height: 20),
-              Container(
-                width: double.infinity,
-                height: 130,
-                decoration: BoxDecoration(
-                  color: kOrange80,
-                  borderRadius: BorderRadius.circular(34),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(kDefaultPadding),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Column(
-                          children: [
-                            Text(
-                              "You haven't been following your regimen.",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1!
-                                  .copyWith(color: Colors.white),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              "You should drink more water and don't forget to take your medicines.",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText2!
-                                  .copyWith(color: Colors.white),
-                            )
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        width: kDefaultPadding,
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          width: 130 - kDefaultPadding,
-                          height: 130 - kDefaultPadding,
-                          decoration: BoxDecoration(
-                              color: kOrange,
-                              borderRadius: BorderRadius.circular(24)),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
+              const RecommendationCard(
+                  title: "You haven't been following your regimen.",
+                  description:
+                      "You should drink more water and don't forget to take your medicines."),
               Padding(
                 padding: const EdgeInsets.only(
                     left: kDefaultPadding, top: kDefaultPadding2x),
@@ -110,72 +67,51 @@ class _SicklerHomeScreenState extends State<SicklerHomeScreen> {
               const SizedBox(
                 height: kDefaultPadding,
               ),
-              Container(
-                width: (screenSize.width - (3 * kDefaultPadding)) * .5,
-                height: 100,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: kDefaultPadding,
-                    vertical: kDefaultPadding * .5),
-                decoration: BoxDecoration(
-                  color: kBlue20,
-                  borderRadius: BorderRadius.circular(24),
-                ),
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      child: Text(
-                        "Water",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText1!
-                            .copyWith(color: kBlue),
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      child: SvgPicture.asset(
-                        "assets/svg/water_drop.svg",
-                        color: kBlue,
-                      ),
-                    ),
-                    Positioned(
-                      left: 0,
-                      bottom: 0,
-                      child: Row(
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                    text: "1.7",
-                                    style:
-                                        Theme.of(context).textTheme.headline1),
-                                TextSpan(
-                                    text: "Litres",
-                                    style:
-                                        Theme.of(context).textTheme.bodyText2),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                      right: 0,
-                      bottom: 9,
-                      child: Text(
-                        "47 min",
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText2!
-                            .copyWith(color: kBlue80, fontSize: 11),
-                      ),
-                    ),
-                  ],
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  StatsCard(
+                    iconLink: "assets/svg/water_drop.svg",
+                    statsTitle: "Water",
+                    statsValue: 1.7,
+                    statsUnit: "Litres",
+                    statsTimestamp: "20 min",
+                    colour: kBlue,
+                    bgColour: kBlue20,
+                  ),
+                  StatsCard(
+                    iconLink: "assets/svg/hb_icon.svg",
+                    statsTitle: "Hb",
+                    statsValue: 12.8,
+                    statsUnit: "g/dl",
+                    statsTimestamp: "Feb 13",
+                    colour: kFuchsia,
+                    bgColour: kFuchsia20,
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  StatsCard(
+                    iconLink: "assets/svg/o2_icon.svg",
+                    statsTitle: "Oxygen",
+                    statsValue: 81,
+                    statsUnit: "%",
+                    statsTimestamp: "28 sec ago",
+                    colour: kOrange,
+                    bgColour: kOrange20,
+                  ),
+                  StatsCard(
+                    iconLink: "assets/svg/drug_icon.svg",
+                    statsTitle: "Drugs",
+                    statsValue: 6,
+                    statsUnit: "pm",
+                    statsTimestamp: "tonight",
+                    colour: kPurple,
+                    bgColour: kPurple20,
+                  ),
+                ],
               ),
               Padding(
                 padding: const EdgeInsets.only(
