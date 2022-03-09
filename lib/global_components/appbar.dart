ee6ecc4ab299565ac4aca879c200bd9ed138320a
nuilewis <lewisnuikweh@gmail.com>
2022-03-09 21:42:39 +0100

    made appbar more versatile with showActions property

--- lib/global_components/appbar.dart
+++ lib/global_components/appbar.dart
@@ -2,39 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sickler/constants.dart';
-import 'package:sickler/size_config.dart';
 
 class SicklerAppBar extends StatelessWidget with PreferredSizeWidget {
   @override
   final Size preferredSize;
   final Color? iconColor;
+  final Widget? leading;
+  final bool? showActions;
 
   const SicklerAppBar({
     Key? key,
     this.preferredSize = const Size.fromHeight(92),
     this.iconColor,
+    this.leading,
+    this.showActions = true,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      leadingWidth: relWidth(kDefaultPadding2x + 50, context),
+      //leadingWidth: relWidth(kDefaultPadding2x + 50, context),
       toolbarHeight: preferredSize.height,
-      actions: [
-        IconButton(
-          padding: const EdgeInsets.only(
-              right: kDefaultPadding, top: kDefaultPadding2x),
-          onPressed: () {
-            HapticFeedback.lightImpact();
-            Feedback.forTap(context);
-            Scaffold.of(context).openEndDrawer();
-          },
-          icon: SvgPicture.asset(
-            "assets/svg/menu_icon.svg",
-            color: iconColor ?? Theme.of(context).iconTheme.color,
-          ),
-        ),
-      ],
+      leading: leading,
+      actions: showActions == true
+          ? [
+              IconButton(
+                padding: const EdgeInsets.only(
+                    right: kDefaultPadding, top: kDefaultPadding2x),
+                onPressed: () {
+                  HapticFeedback.lightImpact();
+                  Feedback.forTap(context);
+                  Scaffold.of(context).openEndDrawer();
+                },
+                icon: SvgPicture.asset(
+                  "assets/svg/menu_icon.svg",
+                  color: iconColor ?? Theme.of(context).iconTheme.color,
+                ),
+              ),
+            ]
+          : null,
     );
   }
 }
