b4cb873d1d1c2d1e11a98aba9c24b53898dbaeb9
nuilewis <lewisnuikweh@gmail.com>
2022-03-10 21:41:09 +0100

    Added coloured sickler button

--- lib/global_components/sickler_button.dart
+++ lib/global_components/sickler_button.dart
@@ -51,3 +51,52 @@ class SicklerButton extends StatelessWidget {
     );
   }
 }
+
+class SicklerColoredButton extends StatelessWidget {
+  final bool? hasShadow;
+  final String buttonLabel;
+  final Color? labelColour;
+  final Color buttonBgColour;
+  final VoidCallback onPressed;
+  const SicklerColoredButton({
+    Key? key,
+    required this.buttonLabel,
+    this.labelColour,
+    required this.buttonBgColour,
+    required this.onPressed,
+    this.hasShadow = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: relHeight(72, context),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          boxShadow: hasShadow!
+              ? [
+                  BoxShadow(
+                      color: buttonBgColour.withOpacity(.3),
+                      offset: const Offset(0, 7),
+                      blurRadius: 15)
+                ]
+              : null,
+          color: buttonBgColour,
+          borderRadius: BorderRadius.circular(kDefaultPadding + 4),
+        ),
+        child: Center(
+          child: Text(
+            buttonLabel,
+            style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: labelColour ??
+                    Theme.of(context).textTheme.bodyText1!.color),
+          ),
+        ),
+      ),
+    );
+  }
+}
