c8ebfad178c9bed795794dc799297bd3987a3a5e
nuilewis <lewisnuikweh@gmail.com>
2022-03-14 07:33:57 +0100

    Edited the app bar

--- lib/global_components/scaffold_body_with_top_image.dart
+++ lib/global_components/scaffold_body_with_top_image.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sickler/global_components/appbar.dart';
 
 import '../constants.dart';
@@ -11,18 +10,17 @@ class SicklerScaffoldBodyWithTopImage extends StatelessWidget {
   final bool? showBackButton;
   final bool? showAppBar;
   final String? pageTitle;
-  final bool showPageTitile;
-  const SicklerScaffoldBodyWithTopImage(
-      {Key? key,
-      required this.child,
-      this.topBgColour,
-      this.topBgImageLink,
-      this.showAppBar = true,
-      this.showBackButton = true,
-      this.pageTitle,
-      this.showPageTitile = false,
-      })
-      : super(key: key);
+  final bool showPageTitle;
+  const SicklerScaffoldBodyWithTopImage({
+    Key? key,
+    required this.child,
+    this.topBgColour,
+    this.topBgImageLink,
+    this.showAppBar = true,
+    this.showBackButton = true,
+    this.pageTitle,
+    this.showPageTitle = false,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -31,15 +29,16 @@ class SicklerScaffoldBodyWithTopImage extends StatelessWidget {
       extendBodyBehindAppBar: true,
       appBar: showAppBar!
           ? SicklerAppBar(
-              leading: showBackButton!
-                  ? Padding(
-                      padding: const EdgeInsets.only(
-                          left: kDefaultPadding, top: kDefaultPadding2x),
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: SvgPicture.asset("assets/svg/back_icon.svg")),
-                    )
-                  : null,
+              showBackButton: showBackButton,
+              // leading: showBackButton!
+              //     ? Padding(
+              //         padding: const EdgeInsets.only(
+              //             left: kDefaultPadding, top: kDefaultPadding2x),
+              //         child: IconButton(
+              //             onPressed: () {},
+              //             icon: SvgPicture.asset("assets/svg/back_icon.svg")),
+              //       )
+              //     : null,
             )
           : null,
       body: SingleChildScrollView(
@@ -58,13 +57,15 @@ class SicklerScaffoldBodyWithTopImage extends StatelessWidget {
               height: screenSize.height * .3,
               width: screenSize.width,
               child: Padding(
-                padding:  EdgeInsets.only(left: kDefaultPadding2x, top: screenSize.height *.15),
-                child: showPageTitile? Text(
-                  
-                  pageTitle!,
-                  textAlign: TextAlign.left,
-                  style: Theme.of(context).textTheme.headline2,
-                ) : null,
+                padding: EdgeInsets.only(
+                    left: kDefaultPadding2x, top: screenSize.height * .15),
+                child: showPageTitle
+                    ? Text(
+                        pageTitle!,
+                        textAlign: TextAlign.left,
+                        style: Theme.of(context).textTheme.headline2,
+                      )
+                    : null,
               ),
             ),
             Padding(
