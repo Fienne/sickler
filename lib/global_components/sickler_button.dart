40ba05b458ab0df6b6b586e8242ba03572bcd4f0
nuilewis <lewisnuikweh@gmail.com>
2022-03-15 19:32:03 +0100

    added transparent button widget

--- lib/global_components/sickler_button.dart
+++ lib/global_components/sickler_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../constants.dart';
 import '../size_config.dart';
@@ -94,6 +95,60 @@ class SicklerColoredButton extends StatelessWidget {
                 fontWeight: FontWeight.bold,
                 color: labelColour ??
                     Theme.of(context).textTheme.bodyText1!.color),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SicklerTransparentButton extends StatelessWidget {
+
+  final String buttonLabel;
+  final Color? colour;
+  final Color buttonBgColour;
+  final VoidCallback onPressed;
+  const SicklerTransparentButton({
+    Key? key,
+    required this.buttonLabel,
+    required this.colour,
+    required this.buttonBgColour,
+    required this.onPressed,
+    
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        height: relHeight(72, context),
+        width: double.infinity,
+        decoration: BoxDecoration(
+        
+          color: buttonBgColour,
+          borderRadius: BorderRadius.circular(kDefaultPadding + 4),
+        ),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                buttonLabel,
+                style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                      fontSize: 18,
+                    
+                      color: colour,
+                    ),
+              ),
+              const SizedBox(
+                width: kDefaultPadding,
+              ),
+              SvgPicture.asset(
+                "assets/svg/plus_icon.svg",
+                color: colour,
+              ),
+            ],
           ),
         ),
       ),
