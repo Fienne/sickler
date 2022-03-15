a06d6f6edc562803d047027516e066253927dd82
nuilewis <lewisnuikweh@gmail.com>
2022-03-15 19:35:08 +0100

    made customizable menu icon to medicine card

--- lib/screens/homescreen/components/medicine_card.dart
+++ lib/screens/homescreen/components/medicine_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../constants.dart';
@@ -11,6 +12,8 @@ class SicklerMedicineCard extends StatelessWidget {
   final String time;
   final String dose;
   final String daysLeft;
+  final bool? showMenuButton;
+  final VoidCallback? menuOnPressed;
 
   const SicklerMedicineCard({
     Key? key,
@@ -18,35 +21,39 @@ class SicklerMedicineCard extends StatelessWidget {
     required this.dose,
     required this.daysLeft,
     required this.time,
+    this.showMenuButton = false,
+    this.menuOnPressed,
     this.colour,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ///Todo: make checkbox work
     return Container(
+      margin: const EdgeInsets.only(top: kDefaultPadding/2, bottom: kDefaultPadding/2),
       width: double.infinity,
       height: relHeight(150, context),
       padding: const EdgeInsets.only(
           left: kDefaultPadding,
-          right: kDefaultPadding,
-          top: kDefaultPadding,
+          right: kDefaultPadding/2,
+         
           bottom: kDefaultPadding / 2),
       decoration: BoxDecoration(
         color: Theme.of(context).cardColor,
-        borderRadius: BorderRadius.circular(24),
+        borderRadius: BorderRadius.circular(28),
       ),
       child: Stack(
         children: [
           Positioned(
             left: 0,
-            top: 0,
+            top: kDefaultPadding,
             child: SvgPicture.asset(
               "assets/svg/drug_icon.svg",
               color: colour ?? kPurple,
             ),
           ),
           Positioned(
-            top: 0,
+            top: kDefaultPadding,
             left: kDefaultPadding2x,
             child: Text(
               medicineName,
@@ -54,8 +61,28 @@ class SicklerMedicineCard extends StatelessWidget {
                   Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 18),
             ),
           ),
+         showMenuButton! ? Positioned(
+            right: 0,
+            top: 0,
+            child: 
+            GestureDetector(
+                  onTap: () {
+                Feedback.forTap(context);
+                HapticFeedback.lightImpact();
+                menuOnPressed;
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(kDefaultPadding),
+                child: SvgPicture.asset(
+                    "assets/svg/horizontal_menu_dot_icon.svg",
+                    color: kDark60,
+                  
+                ),
+              ),
+            ),
+          ) : const SizedBox(),
           Positioned(
-            top: kDefaultPadding2x,
+            top: kDefaultPadding*3,
             left: kDefaultPadding,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -118,7 +145,7 @@ class SicklerMedicineCard extends StatelessWidget {
             ),
           ),
           Positioned(
-            right: 0,
+            right: 4,
             bottom: 0,
             child: SicklerCheckbox(
               colour: colour ?? kPurple,
