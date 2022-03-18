3d88fbd2b0b505d03d9ba57e6656e95470e5c1e7
nuilewis <lewisnuikweh@gmail.com>
2022-03-18 17:23:58 +0100

    Added firebase auth service

---
+++ lib/services/firebase_auth/firebase_auth_service.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthService {
+  void registerUser( String email, String password) {}
+  void signIn() {}
+  void sungOut() {}
+  void reigsterWithGoogle() {}
+  void signInWithGoogle() {}
+}
