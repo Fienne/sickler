45e1e2cb7cf5f6a7110bd28cde756e9d0456d48b
nuilewis <lewisnuikweh@gmail.com>
2022-03-26 06:01:08 +0100

    tried to add animations

--- lib/screens/waterscreen/components/volume_drunk.dart
+++ lib/screens/waterscreen/components/volume_drunk.dart
@@ -2,23 +2,59 @@ import 'package:flutter/material.dart';
 
 import '../../../constants.dart';
 
-class VolumeDrunk extends StatelessWidget {
+class VolumeDrunk extends StatefulWidget {
+  final int? oldVal;
+  final int newVal;
   final String volumeDrunk;
   final String volumeLeft;
   const VolumeDrunk(
-      {Key? key, required this.volumeDrunk, required this.volumeLeft})
+      {Key? key, required this.volumeDrunk, required this.volumeLeft,  this.oldVal, required this.newVal})
       : super(key: key);
 
+  @override
+  State<VolumeDrunk> createState() => _VolumeDrunkState();
+}
+
+class _VolumeDrunkState extends State<VolumeDrunk>
+    with SingleTickerProviderStateMixin {
+  // AnimationController? controller;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   controller = AnimationController(
+  //    // lowerBound: VolumeDrunk,
+  //     upperBound: widget.newVal.toDouble(),
+  //     vsync: this,
+  //     duration: Duration(milliseconds: 300),
+  //   );
+
+  //   //controller!.forward();
+  //   controller!.addListener(() {
+  //     setState(() {
+        
+  //     });
+  //     print(controller!.value);
+  //   });
+  // }
+
+
+// @override
+//   void dispose() {
+//     controller!.dispose();
+//     super.dispose();
+//   }
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Text(
-          volumeDrunk,
+         widget.volumeDrunk,
           style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 36),
         ),
         Text(
-          "Remaining: $volumeLeft ml",
+          "Remaining: ${widget.volumeLeft} ml",
           style:
               Theme.of(context).textTheme.bodyText2!.copyWith(color: kDark60),
         ),
